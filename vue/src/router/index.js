a54c0187efadb4f9fee5fef32ff140c45486eaee
“SHENEL98” <wsakurera@gmail.com>
2023-03-25 21:59:30 +0100

    Redirect to Login/Register Page

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,8 +4,10 @@ import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Surveys from '../views/Surveys.vue';
+import store from '../store';
 
 import DefaultLayout from '../components/DefaultLayout.vue';
+import AuthLayout from '../components/AuthLayout.vue';
 
 
 const routes =[
@@ -14,10 +16,29 @@ const routes =[
         redirect: '/dashboard',
         name: 'Dashboard',
         component: DefaultLayout,
+        meta: { requireAuth: true },
         children: [
             {path: '/dashboard', name: 'Dashboard', component: Dashboard},
             {path: '/survey', name: 'Surveys', component: Surveys}
 
+        ]
+    },
+    {
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: {isGuest: true },
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },{
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
         ]
     },
     {
@@ -34,6 +55,19 @@ const routes =[
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+
+router.beforeEach((to, from, next)=> {
+    if(to.meta.requireAuth && !store.state.user.token){
+        next({name: 'Login'})
+    // }else if(store.state.user.token && (to.name === 'Login' || to.name === 'Register')){
+    }else if(store.state.user.token && (to.meta.isGuest )){
+
+        next({name: 'Dashboard'});
+    }else{
+        next();
+    }
 })
 
 export default router;
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,26 +1,5 @@
-<!--
-  This example requires some changes to your config:
+ <template>
   
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<template>
-    <!--
-      This example requires updating your template:
-  
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    -->
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div class="w-full max-w-md space-y-8">
         <div>
@@ -29,7 +8,7 @@
           <p class="mt-2 text-center text-sm text-gray-600">
             Or
             {{ ' ' }}
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+            <router-link :to="{name: 'Register'}"  class="font-medium text-indigo-600 hover:text-indigo-500">Register for free</router-link>
           </p>
         </div>
         <form class="mt-8 space-y-6" action="#" method="POST">
@@ -49,11 +28,7 @@
             <div class="flex items-center">
               <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
               <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-            </div>
-  
-            <div class="text-sm">
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
-            </div>
+            </div> 
           </div>
   
           <div>
