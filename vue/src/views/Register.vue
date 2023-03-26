6fc17aa6923a7a099b42f091e48b184409b487ec
“SHENEL98” <wsakurera@gmail.com>
2023-03-26 20:37:22 +0200

    Sign in frontend and backend

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -12,21 +12,25 @@
             <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500">Login to your account</router-link>
           </p>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        <form class="mt-8 space-y-6" @submit="register">
           <input type="hidden" name="remember" value="true" />
           <div class="-space-y-px rounded-md shadow-sm">
             <div>
               <label for="fullname" class="sr-only">Full name</label>
-              <input id="fullname" name="name" type="text" autocomplete="name" required="" class="relative block w-full rounded-t-md rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Full name" />
+              <input id="fullname" name="name" type="text" autocomplete="name" required="" v-model="user.name" class="relative block w-full rounded-t-md rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Full name" />
             </div>
             <br>
             <div>
               <label for="email-address" class="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address" />
+              <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email" class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address" />
             </div>
             <div>
               <label for="password" class="sr-only">Password</label>
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
+              <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password" class="relative block w-full   border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
+            </div>
+            <div>
+              <label for="password_confirmation" class="sr-only">Confirmation Password</label>
+              <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password_confirmation" required="" v-model="user.password_confirmation" class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Confirmation Password" />
             </div>
           </div> 
   
@@ -45,4 +49,27 @@
   
   <script setup>
   import { LockClosedIcon } from '@heroicons/vue/20/solid'
+  import store from '../store';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+  //object
+  const user ={
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation:''
+  };
+
+  function register(ev){
+    ev.preventDefault();
+    store
+      .dispatch('register',user)
+      .then((res) => {
+        router.push({
+          name: 'Dashboard'
+        })
+      })
+  }
+
   </script>
