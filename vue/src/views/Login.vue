75c841e8415ca1a70833ee1e7d203e266bdbb175
SHENEL98 <wsakurera@gmail.com>
2023-04-22 00:46:38 +0200

    Show validation errors

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -93,51 +93,80 @@
         </div>
 
         <div>
-          <button
-            type="submit"
-            class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-              <LockClosedIcon
-                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                aria-hidden="true"
-              />
-            </span>
-            Sign in
-          </button>
-        </div>
+      <button
+        type="submit"
+        :disabled="loading"
+        class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        :class="{
+          'cursor-not-allowed': loading,
+          'hover:bg-indigo-500': loading,
+        }"
+      >
+        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+          <LockClosedIcon
+            class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+            aria-hidden="true"
+          />
+        </span>
+        <svg
+          v-if="loading"
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+        Sign in
+      </button>
+    </div>
       </form>
     </div>
   </div>
 </template>
-  
-  <script setup>
+ 
+<script setup>
 import { LockClosedIcon } from "@heroicons/vue/20/solid";
 import store from "../store";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
+import Alert from "../components/Alert.vue";
 
 const router = useRouter();
 
 const user = {
   email: "",
   password: "",
-  remember: false,
 };
-
+let loading = ref(false);
 let errorMsg = ref("");
 
 function login(ev) {
   ev.preventDefault();
 
+  loading.value = true;
   store
     .dispatch("login", user)
     .then(() => {
+      loading.value = false;
       router.push({
         name: "Dashboard",
       });
     })
     .catch((err) => {
+      loading.value = false;
       errorMsg.value = err.response.data.error;
     });
 }
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,75 +1,107 @@
 
-<template>   
- <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+<template>
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 
-      <div class="w-full max-w-md space-y-8">
-        <div>
-          <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register Free</h2>
-          <p class="mt-2 text-center text-sm text-gray-600">
-            Or
-            {{ ' ' }}
-            <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500">Login to your account</router-link>
-          </p>
-        </div>
-        <form class="mt-8 space-y-6" @submit="register">
-          <input type="hidden" name="remember" value="true" />
-          <div class="-space-y-px rounded-md shadow-sm">
-            <div>
-              <label for="fullname" class="sr-only">Full name</label>
-              <input id="fullname" name="name" type="text" autocomplete="name" required="" v-model="user.name" class="relative block w-full rounded-t-md rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Full name" />
-            </div>
-            <br>
-            <div>
-              <label for="email-address" class="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email" class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address" />
-            </div>
-            <div>
-              <label for="password" class="sr-only">Password</label>
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password" class="relative block w-full   border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
-            </div>
-            <div>
-              <label for="password_confirmation" class="sr-only">Confirmation Password</label>
-              <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password_confirmation" required="" v-model="user.password_confirmation" class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Confirmation Password" />
-            </div>
-          </div> 
-  
-          <div>
-            <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-              </span>
-              Sign up
-            </button>
-          </div>
-        </form>
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company" />
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register Free</h2>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Or
+          {{ ' ' }}
+          <router-link :to="{ name: 'Login' }" class="font-medium text-indigo-600 hover:text-indigo-500">Login to your
+            account</router-link>
+        </p>
       </div>
+      <form class="mt-8 space-y-6" @submit="register">
+        <Alert v-if="Object.keys(errors).length" class="flex-col items-stretch text-sm">
+          <div v-for="(field, i) of Object.keys(errors)" :key="i">
+            <div v-for="(error, ind) of errors[field] || []" :key="ind">
+              * {{ error }}
+            </div>
+          </div>
+        </Alert>
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="fullname" class="sr-only">Full name</label>
+            <input id="fullname" name="name" type="text" autocomplete="name" required="" v-model="user.name"
+              class="relative block w-full rounded-t-md rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Full name" />
+          </div>
+          <br>
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email"
+              class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Email address" />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required=""
+              v-model="user.password"
+              class="relative block w-full   border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Password" />
+          </div>
+          <div>
+            <label for="password_confirmation" class="sr-only">Confirmation Password</label>
+            <input id="password_confirmation" name="password_confirmation" type="password"
+              autocomplete="current-password_confirmation" required="" v-model="user.password_confirmation"
+              class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Confirmation Password" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit"
+            class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+            Sign up
+          </button>
+        </div>
+      </form>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
-  import { LockClosedIcon } from '@heroicons/vue/20/solid'
-  import store from '../store';
-  import { useRouter } from 'vue-router';
+<script setup>
+import { LockClosedIcon } from '@heroicons/vue/20/solid'
+import store from '../store';
+import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import Alert from "../components/Alert.vue";
 
-  const router = useRouter();
-  //object
-  const user ={
-    name: '',
-    email: '',
-    password: '',
-    password_confirmation:''
-  };
 
-  function register(ev){
-    ev.preventDefault();
-    store
-      .dispatch('register',user)
-      .then((res) => {
-        router.push({
-          name: 'Dashboard'
-        })
+const router = useRouter();
+//object
+const user = {
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: ''
+};
+
+const loading = ref(false);
+const errors = ref({});
+
+function register(ev) {
+  ev.preventDefault();
+  store
+    .dispatch('register', user)
+    .then((res) => {
+      router.push({
+        name: 'Dashboard'
       })
-  }
+    })
+    .catch((error) => {
+      loading.value = false;
+      if (error.response.status === 422) {
+        errors.value = error.response.data.errors;
+      }
+    });
+}
 
-  </script>
+</script>
